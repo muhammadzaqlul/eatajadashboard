5763f54e4e1901b5cd6815b45048a7110e69752c
Singgih Aji Sasongko <gokonggokong71@gmail.com>
2021-07-17 18:32:24 +0700

    update ads

--- src/views/Ads.vue
+++ src/views/Ads.vue
@@ -6,23 +6,59 @@
     </div>
     <div class="ma-10">
       <v-row>
-        <v-col v-for="(banner, index) in banners" :key="index" sm="12" md="4">
+        <v-col :key="index" sm="12" md="4">
           <h1>Ads</h1>
           <v-img
             max-height="100%"
             max-width="100%"
-            :src="`https://api.eataja.com/storage/${banner.banner_photo}`"
+            :src="`https://api.eataja.com/storage/${banners[0].banner_photo}`"
             class="ml-auto"
           ></v-img>
           <v-file-input
             runcate-length="15"
             placeholder="Input Photo"
             accept="image/*"
-            v-model="photo"
-            @change="photo = e.target.file[0]"
+            v-model="firstPhoto"
+            @change="firstPhoto = e.target.file[0]"
           >
           </v-file-input>
-          <v-btn @click="upload(banner.id)" class="primary">Upload</v-btn>
+          <v-btn @click="upload(2)" class="primary">Upload</v-btn>
+        </v-col>
+        <v-col :key="index" sm="12" md="4">
+          <h1>Ads</h1>
+          <v-img
+            max-height="100%"
+            max-width="100%"
+            :src="`https://api.eataja.com/storage/${banners[1].banner_photo}`"
+            class="ml-auto"
+          ></v-img>
+          <v-file-input
+            runcate-length="15"
+            placeholder="Input Photo"
+            accept="image/*"
+            v-model="secondPhoto"
+            @change="secondPhoto = e.target.file[0]"
+          >
+          </v-file-input>
+          <v-btn @click="upload(3)" class="primary">Upload</v-btn>
+        </v-col>
+        <v-col :key="index" sm="12" md="4">
+          <h1>Ads</h1>
+          <v-img
+            max-height="100%"
+            max-width="100%"
+            :src="`https://api.eataja.com/storage/${banners[2].banner_photo}`"
+            class="ml-auto"
+          ></v-img>
+          <v-file-input
+            runcate-length="15"
+            placeholder="Input Photo"
+            accept="image/*"
+            v-model="thirdPhoto"
+            @change="thirdPhoto = e.target.file[0]"
+          >
+          </v-file-input>
+          <v-btn @click="upload(4)" class="primary">Upload</v-btn>
         </v-col>
       </v-row>
     </div>
@@ -39,7 +75,10 @@ export default {
   data() {
     return {
       banners: [],
-      photo: ""
+      photo: "",
+      firstPhoto: "",
+      secondPhoto: "",
+      thirdPhoto: ""
     };
   },
   mounted() {
@@ -55,8 +94,13 @@ export default {
   methods: {
     upload(id) {
         let formData = new FormData();
-        formData.append('photo', this.photo);
-
+        if(id === 2){
+            formData.append('photo', this.firstPhoto)
+        } else if (id === 3){
+            formData.append('photo', this.secondPhoto)
+        } else {
+            formData.append('photo', this.thirdPhoto)
+        }
 
       axios
         .post("https://api.eataja.com/api/update-banner/" + id, formData, { headers: {"Content-Type": "multipart/form-data"} })
